b8fe8d49d6924887ee4830a364c22a4409ca615b
Halleluyah Adesuyi <adesuyihalleluyah@gmail.com>
2024-06-06 19:22:47 +0100

    Conditionally render "Add To Cart" button; style it

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
+import { formatCurrency } from "../utilities/formatCurrency"
 
 type StoreItemProps = {
     id: number
@@ -8,8 +9,9 @@ type StoreItemProps = {
 }
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+    const quantity = 0
     return (
-        <Card>
+        <Card className="h-100">
             <Card.Img
                 variant="top"
                 src={imgUrl}
@@ -19,8 +21,14 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
             <Card.Body className="d-flex flex-column">
                 <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
                     <span className="fs-2">{name}</span>
-                    <span className="ms-2 text-muted">{price}</span>
+                    <span className="ms-2 text-muted">{formatCurrency(price)}</span>
                 </Card.Title>
+                <div className="mt-auto">
+                    {quantity === 0 ? (
+                        <Button className="w-100">+ Add To Cart</Button>
+                        ) : null
+                    }
+                </div>
             </Card.Body>
         </Card>
 
