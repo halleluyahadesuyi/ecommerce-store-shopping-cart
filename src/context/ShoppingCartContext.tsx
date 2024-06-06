d846bef569423be4d08a41fece7a78d99c85fe18
Halleluyah Adesuyi <adesuyihalleluyah@gmail.com>
2024-06-07 00:28:48 +0100

    Add the provider part of context

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,7 +1,18 @@
-import { createContext, useContext } from "react";
+import { ReactNode, createContext, useContext } from "react";
 
+type ShoppingCartProviderProps = {
+    children: ReactNode
+}
 const ShoppingCartContext = createContext({})
 
 export function useShoppingCart() {
     return useContext(ShoppingCartContext)
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+    return (
+        <ShoppingCartContext.Provider value={{}}>
+            {children}
+        </ShoppingCartContext.Provider>
+    )
 }
