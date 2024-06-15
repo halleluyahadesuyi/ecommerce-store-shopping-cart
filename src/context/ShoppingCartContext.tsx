a27b960f4f5a7d78cca3c008abd5ff27696a1279
Halleluyah Adesuyi <adesuyihalleluyah@gmail.com>
2024-06-15 05:10:17 +0100

    feat: Add bootstrap slider to cart

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, createContext, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
 
 // Declare data types of ShoppingCartProvider's props i.e. children
 type ShoppingCartProviderProps = {
@@ -117,6 +118,7 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
       }}
     >
       {children}
+      <ShoppingCart />
     </ShoppingCartContext.Provider>
   );
 }
